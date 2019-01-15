5ed02dfd94189c3ab39447c60042864d525b73a4
hellokj <neil1996xyz@gmail.com>
2019-01-15 10:49:48 +0800

    homework_3

--- app/src/main/java/com/home/currency/MainActivity.java
+++ app/src/main/java/com/home/currency/MainActivity.java
@@ -1,52 +1,88 @@
 package com.home.currency;
 
-import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
     private EditText edNtd;
+    private TextView tvUsd;
+    private TextView tvJpy;
+    private float rate_usd2ntd;
+    private float rate_jpy2ntd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
+        setRates();
+    }
+
+    private void setRates(){
+        rate_usd2ntd = 30.9f;
+        rate_jpy2ntd = 0.26f;
     }
 
     private void findViews() {
+        tvUsd = findViewById(R.id.usd);
+        tvJpy = findViewById(R.id.jpy);
         edNtd = findViewById(R.id.ntd);
+        Button rate = findViewById(R.id.rate);
+        rate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle(R.string.rate)
+                        .setMessage(getString(R.string.usd2ntd) + rate_usd2ntd + "\n" +
+                                    getString(R.string.jpy2ntd) + rate_jpy2ntd)
+                        .setPositiveButton(R.string.ok,null)
+                        .show();
+            }
+        });
     }
 
     public void exchange(View view){
         String input = edNtd.getText().toString();
         if(input.equals("")){
             new AlertDialog.Builder(this)
-                    .setTitle("Problem")
-                    .setMessage("Please enter your NTD amount")
-                    .setPositiveButton("OK", null)
+                    .setTitle(R.string.problem)
+                    .setMessage(R.string.input_ntd)
+                    .setPositiveButton(R.string.ok, null)
                     .show();
         } else {
             float ntd = Float.parseFloat(input);
             float usd = getUsd(ntd);
-            new AlertDialog.Builder(this)
+            float jpy = getJpy(ntd);
+//            修改為輸出至editText內容，取代彈出視窗
+            tvUsd.setText(String.valueOf(usd));
+//            setContentView(tvUsd);
+            tvJpy.setText(String.valueOf(jpy));
+//            setContentView(tvJpy);
+            /*new AlertDialog.Builder(MainActivity.this)
                     .setTitle("Result")
-                    .setMessage("USD is " + usd)
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            edNtd.setText("");
-                        }
-                    })
-                    .show();
+                    .setMessage("Calculate Finished.")
+                    .setPositiveButton("OK", null)
+                    .show();*/
+            }
         }
+
+    public void reset(View view){
+        edNtd.setText("");
+        tvUsd.setText("");
+        tvJpy.setText("");
+    }
+
+    private float getJpy(float ntd){
+        return ntd/rate_jpy2ntd;
     }
 
     private float getUsd(float ntd) {
-        float rate_usd2ntd = 30.9f;
         return ntd/rate_usd2ntd;
     }
 }
